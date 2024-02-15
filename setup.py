d166d2b35a11a30664047c53d6c3587a15c06b9c
Lauren Aldoroty <laurenaldoroty@gmail.com>
2024-02-15 14:31:06 -0500

    change package name in setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(name='ropeat',
+setup(name='phrosty',
         version='v0.0.0',
         packages=find_packages(),
-        package_data={'ropeat/models': ['*.pkl']},
+        # package_data={'ropeat/models': ['*.pkl']},
         include_package_data=True)
