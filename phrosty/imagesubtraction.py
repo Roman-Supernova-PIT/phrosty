e182ec1b7272887d5dcbb8bea63d10366238af84
Lauren Aldoroty <laurenaldoroty@gmail.com>
2024-09-06 07:53:20 -0700

    aesthetic changes

--- phrosty/imagesubtraction.py
+++ phrosty/imagesubtraction.py
@@ -163,7 +163,7 @@ def get_imsim_psf(ra,dec,band,pointing,sca,size=201,out_path=output_files_rootdi
 
     logger = _logger if logger is None else logger
 
-    savedir = os.path.join(output_files_rootdir,'psf')
+    savedir = os.path.join(out_path,'psf')
     check_and_mkdir(savedir)
     savename = f'psf_{ra}_{dec}_{band}_{pointing}_{sca}.fits'
     savepath = os.path.join(savedir,savename)
@@ -251,11 +251,11 @@ def rotate_psf(ra,dec,psf,target,savename=None,force=False,verbose=False):
     return psf_path
 
 def crossconvolve(sci_img_path, sci_psf_path,
-                    ref_img_path, ref_psf_path,
-                    force=False,verbose=False,
-                    sci_outname=None,
-                    ref_outname=None,
-                    out_path=output_files_rootdir):
+                  ref_img_path, ref_psf_path,
+                  force=False,verbose=False,
+                  sci_outname=None,
+                  ref_outname=None,
+                  out_path=output_files_rootdir):
 
     savedir = os.path.join(out_path,'convolved')
     check_and_mkdir(savedir)
@@ -275,7 +275,7 @@ def crossconvolve(sci_img_path, sci_psf_path,
         savepaths.append(savepath)
 
     do_conv = (force is True) or (force is False and not any([os.path.exists(p) for p in savepaths]))
-    skip_conv =  (not force) and all([os.path.exists(p) for p in savepaths])
+    skip_conv = (not force) and all([os.path.exists(p) for p in savepaths])
     if do_conv:
         for img, psf, name, save in zip([sci_img_path, ref_img_path],
                             [ref_psf_path, sci_psf_path],
@@ -285,7 +285,7 @@ def crossconvolve(sci_img_path, sci_psf_path,
             psfdata = fits.getdata(psf, ext=0).T
 
             convolved = convolve_fft(imgdata, psfdata, boundary='fill', nan_treatment='fill', \
-                                    fill_value=0.0, normalize_kernel=True, preserve_nan=True, allow_huge=True)
+                                     fill_value=0.0, normalize_kernel=True, preserve_nan=True, allow_huge=True)
 
             with fits.open(img) as hdl:
                 hdl[0].data[:, :] = convolved.T
@@ -314,8 +314,8 @@ def stampmaker(ra, dec, imgpath, savedir=None, savename=None, shape=np.array([10
     x, y = skycoord_to_pixel(coord, wcs)
     pxradec = np.array([[x,y]])
     
-    Stamp_Generator.SG(FITS_obj=imgpath, COORD=pxradec, COORD_TYPE='IMAGE', \
-            STAMP_IMGSIZE=shape, FILL_VALUE=np.nan, FITS_StpLst=savepath)
+    Stamp_Generator.SG(FITS_obj=imgpath, COORD=pxradec, COORD_TYPE='IMAGE',
+                       STAMP_IMGSIZE=shape, FILL_VALUE=np.nan, FITS_StpLst=savepath)
 
     return savepath
 
@@ -328,18 +328,18 @@ def bkg_mask(imgpath):
     'FLUX_RADIUS', 'FWHM_IMAGE', 'A_IMAGE', 'B_IMAGE', 'KRON_RADIUS', 'THETA_IMAGE', 'SNR_WIN']
 
     scatalog = PY_SEx.PS(FITS_obj=imgpath, SExParam=source_ext_params, GAIN_KEY='GAIN', SATUR_KEY='SATURATE', \
-                            BACK_TYPE='MANUAL', BACK_VALUE=0.0, BACK_SIZE=64, BACK_FILTERSIZE=3, DETECT_THRESH=1.5, \
-                            DETECT_MINAREA=5, DETECT_MAXAREA=0, DEBLEND_MINCONT=0.001, BACKPHOTO_TYPE='LOCAL', \
-                            CHECKIMAGE_TYPE='SEGMENTATION', AddRD=True, ONLY_FLAGS=None, XBoundary=0.0, YBoundary=0.0, \
-                            DEFAULT_GAIN=1.0, DEFAULT_SATUR=100000, MDIR=None, VERBOSE_LEVEL=1)[1][0]
+                         BACK_TYPE='MANUAL', BACK_VALUE=0.0, BACK_SIZE=64, BACK_FILTERSIZE=3, DETECT_THRESH=1.5, \
+                         DETECT_MINAREA=5, DETECT_MAXAREA=0, DEBLEND_MINCONT=0.001, BACKPHOTO_TYPE='LOCAL', \
+                         CHECKIMAGE_TYPE='SEGMENTATION', AddRD=True, ONLY_FLAGS=None, XBoundary=0.0, YBoundary=0.0, \
+                         DEFAULT_GAIN=1.0, DEFAULT_SATUR=100000, MDIR=None, VERBOSE_LEVEL=1)[1][0]
 
     bkg_mask = (scatalog == 0)
 
     return bkg_mask
 
 def difference(scipath, refpath, 
-        scipsfpath, refpsfpath, out_path=output_files_rootdir, savename=None, ForceConv='REF', GKerHW=9, KerPolyOrder=2, BGPolyOrder=0, 
-        ConstPhotRatio=True, backend='Numpy', cudadevice='0', nCPUthreads=1, force=False, verbose=False, logger=None):
+               scipsfpath, refpsfpath, out_path=output_files_rootdir, savename=None, ForceConv='REF', GKerHW=9, KerPolyOrder=2, BGPolyOrder=0, 
+               ConstPhotRatio=True, backend='Numpy', cudadevice='0', nCPUthreads=1, force=False, verbose=False, logger=None):
 
     tracemalloc.start()
 
@@ -391,10 +391,10 @@ def difference(scipath, refpath,
 
         # Do SFFT subtraction
         Customized_Packet.CP(FITS_REF=refpath, FITS_SCI=scipath, FITS_mREF=ref_masked_savepath, FITS_mSCI=sci_masked_savepath, \
-                            ForceConv=ForceConv, GKerHW=GKerHW, FITS_DIFF=diff_savepath, FITS_Solution=soln_savepath, \
-                            KerPolyOrder=KerPolyOrder, BGPolyOrder=BGPolyOrder, ConstPhotRatio=ConstPhotRatio, \
-                            BACKEND_4SUBTRACT=backend, CUDA_DEVICE_4SUBTRACT=cudadevice, \
-                            NUM_CPU_THREADS_4SUBTRACT=nCPUthreads,logger=logger)
+                             ForceConv=ForceConv, GKerHW=GKerHW, FITS_DIFF=diff_savepath, FITS_Solution=soln_savepath, \
+                             KerPolyOrder=KerPolyOrder, BGPolyOrder=BGPolyOrder, ConstPhotRatio=ConstPhotRatio, \
+                             BACKEND_4SUBTRACT=backend, CUDA_DEVICE_4SUBTRACT=cudadevice, \
+                             NUM_CPU_THREADS_4SUBTRACT=nCPUthreads,logger=logger)
 
         size,peak = tracemalloc.get_traced_memory()
         logger.debug(f'MEMORY IN imagesubtraction.difference() AFTER Customized_Packet.CP: size = {size}, peak = {peak}')
@@ -406,8 +406,8 @@ def difference(scipath, refpath,
     return diff_savepath, soln_savepath
 
 def decorr_kernel(scipath, refpath, 
-            scipsfpath, refpsfpath,
-            diffpath, solnpath, out_path=output_files_rootdir, savename=None):
+                  scipsfpath, refpsfpath,
+                  diffpath, solnpath, out_path=output_files_rootdir, savename=None):
 
     savedir = os.path.join(out_path, 'dcker')
     check_and_mkdir(savedir)
@@ -436,9 +436,9 @@ def decorr_kernel(scipath, refpath,
     MKerStack = Realize_MatchingKernel(XY_q).FromFITS(FITS_Solution=solnpath)
     MK_Fin = MKerStack[0]
 
-    DCKer = DeCorrelation_Calculator.DCC(MK_JLst = [ref_psf], SkySig_JLst=[sci_bkg], \
-                                        MK_ILst=[sci_psf], SkySig_ILst=[ref_bkg], MK_Fin=MK_Fin, \
-                                        KERatio=2.0, VERBOSE_LEVEL=2)
+    DCKer = DeCorrelation_Calculator.DCC(MK_JLst=[ref_psf], SkySig_JLst=[sci_bkg],
+                                         MK_ILst=[sci_psf], SkySig_ILst=[ref_bkg], 
+                                         MK_Fin=MK_Fin, KERatio=2.0, VERBOSE_LEVEL=2)
 
     with fits.open(scipath) as hdu:
         hdu[0].data = DCKer.T
@@ -447,7 +447,7 @@ def decorr_kernel(scipath, refpath,
     return decorr_savepath
 
 def decorr_img(imgpath, dckerpath, out_path=output_files_rootdir, savename=None):
-    
+
     savedir = os.path.join(out_path,'decorr')
     check_and_mkdir(savedir)
 
@@ -461,9 +461,9 @@ def decorr_img(imgpath, dckerpath, out_path=output_files_rootdir, savename=None)
     DCKer = fits.getdata(dckerpath)
 
     # Final decorrelated difference image: 
-    dcdiff = convolve_fft(img_data, DCKer, boundary='fill', \
-                            nan_treatment='fill', fill_value=0.0, normalize_kernel=True,
-                            preserve_nan=True)
+    dcdiff = convolve_fft(img_data, DCKer, boundary='fill',
+                          nan_treatment='fill', fill_value=0.0, 
+                          normalize_kernel=True, preserve_nan=True)
 
     with fits.open(imgpath) as hdu:
         hdu[0].data[:, :] = dcdiff.T
@@ -507,7 +507,7 @@ def swarp_coadd(imgpath_list,refpath,out_name,out_path=output_files_rootdir,subd
     coadd_savepath = os.path.join(coadd_savedir,out_name)
     
     coadd = PY_SWarp.Coadd(FITS_obj=imgpaths, FITS_ref=refpath, ConfigDict=cd,
-                            OUT_path=coadd_savepath, FILL_VALUE=np.nan)
+                           OUT_path=coadd_savepath, FILL_VALUE=np.nan)
 
     return coadd_savepath, imgpaths
 
