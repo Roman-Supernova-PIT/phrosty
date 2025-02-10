fe8c356d98f7c3d9c9f64ca995658ed8936e926e
Lauren Aldoroty <laurenaldoroty@gmail.com>
2025-02-10 10:39:20 -0800

    make arguments for forced_phot explicit

--- phrosty/photometry.py
+++ phrosty/photometry.py
@@ -89,10 +89,14 @@ def psf_phot(scienceimage,psf,init_params,wcs=None,
     if 'flux' not in init_params.colnames:
         init_params.rename_column('aperture_sum','flux')
 
-    if forced_phot:
+    if forced_phot == True:
         print('x, y are fixed!')
         psf.x_0.fixed = True
         psf.y_0.fixed = True
+    elif forced_phot == False:
+        print('x, y are fitting parameters!')
+        psf.x_0.fixed = False
+        psf.x_0.fixed = False
 
     try: 
         bkgfunc = LocalBackground(15,25,MMMBackground())
