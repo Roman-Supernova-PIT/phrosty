24ce903e9fef70c6bbb901b86592bd3c65bda72a
Rob Knop (Nersc) <raknop@lbl.gov>
2025-08-26 07:52:59 -0700

    comments

--- phrosty/pipeline.py
+++ phrosty/pipeline.py
@@ -925,11 +925,13 @@ def main():
             SNLogger.warning( f"Given Dec {args.dec} is far from DiaObject nominal Dec {diaobj.dec}" )
         diaobj.dec = args.dec
 
-    # Read the image lists
-
+    # Get the image collection
+        
     imgcol = ImageCollection.get_collection( collection=args.image_collection, subset=args.image_subset,
                                              base_path=args.base_path )
 
+    # Create and launch the pipeline
+    
     pipeline = Pipeline( diaobj, imgcol, args.band,
                          science_csv=args.science_images, template_csv=args.template_images,
                          nprocs=args.nprocs, nwrite=args.nwrite, verbose=args.verbose )
