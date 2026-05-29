00708f29bc963bc1025f3dd1acab3d3cce6af634
Lauren Aldoroty <laurenaldoroty@gmail.com>
2026-05-29 10:43:11 -0700

    update slurm sh file and config

--- examples/perlmutter/20172782_slurm_demo.sh
+++ examples/perlmutter/20172782_slurm_demo.sh
@@ -3,10 +3,11 @@
 #SBATCH --constraint gpu
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 32
-###SBATCH --mem 60G
+#SBATCH --mem 40G
 #SBATCH --gpus-per-task 1
 #SBATCH --qos shared
-#SBATCH --time 00:20:00
+#SBATCH --time 00:30:00
+#SBATCH --job-name 20172782_slurm_demo
 #SBATCH --output 20172782_slurm_demo.out
 
 echo 'phrosty slurm job starting at' `date`
@@ -20,31 +21,37 @@ echo
 podman-hpc run --gpu \
     --mount type=bind,source=$PWD,target=/home \
     --mount type=bind,source=$PWD/phrosty,target=/phrosty \
-    --mount type=bind,source=$PWD/dia_out_dir,target=/dia_out_dir \
-    --mount type=bind,source=$PWD/lc_out_dir,target=/lc_out_dir \
+    --mount type=bind,source=$SCRATCH/phrosty_intermediate,target=/scratch \
+    --mount type=bind,source=$PWD/sn_info_dir,target=/sn_info_dir \
+    --mount type=bind,source=$SCRATCH/dia_out_dir,target=/dia_out_dir \
+    --mount type=bind,source=$SCRATCH/lc_out_dir,target=/lc_out_dir \
     --mount type=bind,source=$SCRATCH/phrosty_temp,target=/phrosty_temp \
-    --mount type=bind,source=/dvs_ro/cfs/cdirs/lsst/shared/external/roman-desc-sims/Roman_data,target=/ou2024 \
-    --mount type=bind,source=/dvs_ro/cfs/cdirs/lsst/www/DESC_TD_PUBLIC/Roman+DESC/PQ+HDF5_ROMAN+LSST_LARGE,target=/ou2024_snana \
-    --mount type=bind,source=/dvs_ro/cfs/cdirs/lsst/www/DESC_TD_PUBLIC/Roman+DESC/ROMAN+LSST_LARGE_SNIa-normal,target=/ou2024_snana_lc_dir \
-    --mount type=bind,source=/dvs_ro/cfs/cdirs/lsst/www/DESC_TD_PUBLIC/Roman+DESC/sims_sed_library,target=/ou2024_sims_sed_library \
+    --mount type=bind,source=/global/cfs/cdirs/lsst/shared/external/roman-desc-sims/Roman_data,target=/sims_dir \
+    --mount type=bind,source=/global/cfs/cdirs/lsst/www/DESC_TD_PUBLIC/Roman+DESC/PQ+HDF5_ROMAN+LSST_LARGE,target=/snana_pq_dir \
+    --mount type=bind,source=/global/cfs/cdirs/lsst/www/DESC_TD_PUBLIC/Roman+DESC/ROMAN+LSST_LARGE_SNIa-normal,target=/snid_lc_dir \
     --env LD_LIBRARY_PATH=/usr/lib64:/usr/lib/x86_64-linux-gnu:/usr/local/cuda/lib64:/usr/local/cuda/lib64/stubs \
-    --env PYTHONPATH=/phrosty:/roman_imsim \
+    --env PYTHONPATH=/roman_imsim:/phrosty:/sfft \
     --env OPENBLAS_NUM_THREADS=1 \
     --env MKL_NUM_THREADS=1 \
     --env NUMEXPR_NUM_THREADS=1 \
     --env OMP_NUM_THREADS=1 \
     --env VECLIB_MAXIMUM_THREADS=1 \
+    --env SIMS_DIR=/sims_dir \
+    --env SNANA_PQ_DIR=/snana_pq_dir \
     --env TERM=xterm \
     -w /home \
     -it \
     registry.nersc.gov/m4385/rknop/roman-snpit-env:cuda-dev \
     python phrosty/phrosty/pipeline.py \
       -c phrosty/examples/perlmutter/phrosty_config.yaml \
-      --oc ou2024 \
+      -oc ou2024 \
+      -ic ou2024 \
       --oid 20172782 \
+      -r 11.728845489889757 \
+      -d -42.702185894608725 \
       -b R062 \
-      --ic ou2024
       -t phrosty/examples/perlmutter/20172782_instances_templates_1.csv \
       -s phrosty/examples/perlmutter/20172782_instances_science.csv \
       -p 9 \
-      -w 5
+      -w 5 \
+      -v
