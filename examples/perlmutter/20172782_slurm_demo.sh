f2fc7735f7b0f11dbdaffdc0c2241b46ba8bf094
Lauren Aldoroty <laurenaldoroty@gmail.com>
2026-06-02 10:00:22 -0700

    update slurm example

--- examples/perlmutter/20172782_slurm_demo.sh
+++ examples/perlmutter/20172782_slurm_demo.sh
@@ -20,26 +20,28 @@ echo
 
 podman-hpc run --gpu \
     --mount type=bind,source=$PWD,target=/home \
-    --mount type=bind,source=$PWD/phrosty,target=/phrosty \
-    --mount type=bind,source=$SCRATCH/phrosty_intermediate,target=/scratch \
-    --mount type=bind,source=$PWD/sn_info_dir,target=/sn_info_dir \
-    --mount type=bind,source=$SCRATCH/dia_out_dir,target=/dia_out_dir \
-    --mount type=bind,source=$SCRATCH/lc_out_dir,target=/lc_out_dir \
-    --mount type=bind,source=$SCRATCH/phrosty_temp,target=/phrosty_temp \
-    --mount type=bind,source=/global/cfs/cdirs/lsst/shared/external/roman-desc-sims/Roman_data,target=/sims_dir \
-    --mount type=bind,source=/global/cfs/cdirs/lsst/www/DESC_TD_PUBLIC/Roman+DESC/PQ+HDF5_ROMAN+LSST_LARGE,target=/snana_pq_dir \
-    --mount type=bind,source=/global/cfs/cdirs/lsst/www/DESC_TD_PUBLIC/Roman+DESC/ROMAN+LSST_LARGE_SNIa-normal,target=/snid_lc_dir \
+    --mount type=bind,source=$HOME/secrets,target=/secrets \
+    --mount type=bind,source=$PSCRATCH/snpit_temp,target=/snpit_temp \
+    --mount type=bind,source=$PSCRATCH,target=/scratch \
+    --mount type=bind,source=/dvs_ro/cfs/cdirs/m4385/env,target=/snpit_env \
+    --mount type=bind,source=/pscratch/sd/m/masao/roman_snpit,target=/roman_snpit_masao_scratch \
+    --mount type=bind,source=/pscratch/sd/m/masao/roman_snpit/database_dirs,target=/data \
+    --mount type=bind,source=/dvs_ro/cfs/cdirs/lsst/shared/external/roman-desc-sims/Roman_data,target=/ou2024 \
+    --mount type=bind,source=/dvs_ro/cfs/cdirs/lsst/www/DESC_TD_PUBLIC/Roman+DESC/PQ+HDF5_ROMAN+LSST_LARGE,target=/ou2024_snana \
+    --mount type=bind,source=/dvs_ro/cfs/cdirs/lsst/www/DESC_TD_PUBLIC/Roman+DESC/ROMAN+LSST_LARGE_SNIa-normal,target=/ou2024_snana_lc_dir \
+    --mount type=bind,source=/dvs_ro/cfs/cdirs/lsst/www/DESC_TD_PUBLIC/Roman+DESC/sims_sed_library,target=/ou2024_sims_sed_library \
+    --mount type=bind,source=/dvs_ro/cfs/cdirs/m4385/calib_data/A25ePSF,target=/a25epsf \
     --env LD_LIBRARY_PATH=/usr/lib64:/usr/lib/x86_64-linux-gnu:/usr/local/cuda/lib64:/usr/local/cuda/lib64/stubs \
-    --env PYTHONPATH=/roman_imsim:/phrosty:/sfft \
+    --env PYTHONPATH=/roman_imsim \
     --env OPENBLAS_NUM_THREADS=1 \
     --env MKL_NUM_THREADS=1 \
     --env NUMEXPR_NUM_THREADS=1 \
     --env OMP_NUM_THREADS=1 \
     --env VECLIB_MAXIMUM_THREADS=1 \
-    --env SIMS_DIR=/sims_dir \
-    --env SNANA_PQ_DIR=/snana_pq_dir \
     --env TERM=xterm \
-    -w /home \
+    --env SNPIT_DEFAULT_CONFIG=/snpit_env/configs/nov2025_container_config.yaml \
+    --env SNPIT_CONFIG=/snpit_env/configs/nov2025_container_config.yaml \
+    --annotation run.oci.keep_original_groups=1 \
     -it \
     registry.nersc.gov/m4385/rknop/roman-snpit-env:cuda-dev \
     python phrosty/phrosty/pipeline.py \
