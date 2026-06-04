6161881f35949f491586441a6c97413855f5bc2e
Lauren Aldoroty <laurenaldoroty@gmail.com>
2026-06-04 12:43:41 -0700

    slurm demo works!

--- examples/perlmutter/20172782_slurm_demo.sh
+++ examples/perlmutter/20172782_slurm_demo.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task 32
 #SBATCH --mem 40G
 #SBATCH --gpus-per-task 1
-#SBATCH --qos shared
+#SBATCH --qos debug # or shared, but debug goes faster. But is limited to 30 min. 
 #SBATCH --time 00:30:00
 #SBATCH --job-name 20172782_slurm_demo
 #SBATCH --output 20172782_slurm_demo.out
@@ -19,41 +19,29 @@ podman-hpc images
 echo
 
 podman-hpc run --gpu \
-    --mount type=bind,source=$PWD,target=/home \
-    --mount type=bind,source=$HOME/secrets,target=/secrets \
-    --mount type=bind,source=$PSCRATCH/snpit_temp,target=/snpit_temp \
-    --mount type=bind,source=$PSCRATCH,target=/scratch \
-    --mount type=bind,source=/dvs_ro/cfs/cdirs/m4385/env,target=/snpit_env \
-    --mount type=bind,source=/pscratch/sd/m/masao/roman_snpit,target=/roman_snpit_masao_scratch \
-    --mount type=bind,source=/pscratch/sd/m/masao/roman_snpit/database_dirs,target=/data \
-    --mount type=bind,source=/dvs_ro/cfs/cdirs/lsst/shared/external/roman-desc-sims/Roman_data,target=/ou2024 \
-    --mount type=bind,source=/dvs_ro/cfs/cdirs/lsst/www/DESC_TD_PUBLIC/Roman+DESC/PQ+HDF5_ROMAN+LSST_LARGE,target=/ou2024_snana \
-    --mount type=bind,source=/dvs_ro/cfs/cdirs/lsst/www/DESC_TD_PUBLIC/Roman+DESC/ROMAN+LSST_LARGE_SNIa-normal,target=/ou2024_snana_lc_dir \
-    --mount type=bind,source=/dvs_ro/cfs/cdirs/lsst/www/DESC_TD_PUBLIC/Roman+DESC/sims_sed_library,target=/ou2024_sims_sed_library \
-    --mount type=bind,source=/dvs_ro/cfs/cdirs/m4385/calib_data/A25ePSF,target=/a25epsf \
-    --env LD_LIBRARY_PATH=/usr/lib64:/usr/lib/x86_64-linux-gnu:/usr/local/cuda/lib64:/usr/local/cuda/lib64/stubs \
-    --env PYTHONPATH=/roman_imsim \
-    --env OPENBLAS_NUM_THREADS=1 \
-    --env MKL_NUM_THREADS=1 \
-    --env NUMEXPR_NUM_THREADS=1 \
-    --env OMP_NUM_THREADS=1 \
-    --env VECLIB_MAXIMUM_THREADS=1 \
-    --env TERM=xterm \
-    --env SNPIT_DEFAULT_CONFIG=/snpit_env/configs/nov2025_container_config.yaml \
-    --env SNPIT_CONFIG=/snpit_env/configs/nov2025_container_config.yaml \
-    --annotation run.oci.keep_original_groups=1 \
-    -it \
-    registry.nersc.gov/m4385/rknop/roman-snpit-env:cuda-dev \
-    python phrosty/phrosty/pipeline.py \
-      -c phrosty/examples/perlmutter/phrosty_config.yaml \
-      -oc ou2024 \
-      -ic ou2024 \
-      --oid 20172782 \
-      -r 11.728845489889757 \
-      -d -42.702185894608725 \
-      -b R062 \
-      -t phrosty/examples/perlmutter/20172782_instances_templates_1.csv \
-      -s phrosty/examples/perlmutter/20172782_instances_science.csv \
-      -p 9 \
-      -w 5 \
-      -v
+--mount type=bind,source=$PWD,target=/home \
+--mount type=bind,source=$HOME/secrets,target=/secrets \
+--mount type=bind,source=$PSCRATCH/snpit_temp,target=/snpit_temp \
+--mount type=bind,source=$PSCRATCH,target=/scratch \
+--mount type=bind,source=/dvs_ro/cfs/cdirs/m4385/env,target=/snpit_env \
+--mount type=bind,source=/pscratch/sd/m/masao/roman_snpit,target=/roman_snpit_masao_scratch \
+--mount type=bind,source=/pscratch/sd/m/masao/roman_snpit/database_dirs,target=/data \
+--mount type=bind,source=/dvs_ro/cfs/cdirs/lsst/shared/external/roman-desc-sims/Roman_data,target=/ou2024 \
+--mount type=bind,source=/dvs_ro/cfs/cdirs/lsst/www/DESC_TD_PUBLIC/Roman+DESC/PQ+HDF5_ROMAN+LSST_LARGE,target=/ou2024_snana \
+--mount type=bind,source=/dvs_ro/cfs/cdirs/lsst/www/DESC_TD_PUBLIC/Roman+DESC/ROMAN+LSST_LARGE_SNIa-normal,target=/ou2024_snana_lc_dir \
+--mount type=bind,source=/dvs_ro/cfs/cdirs/lsst/www/DESC_TD_PUBLIC/Roman+DESC/sims_sed_library,target=/ou2024_sims_sed_library \
+--mount type=bind,source=/dvs_ro/cfs/cdirs/m4385/calib_data/A25ePSF,target=/a25epsf \
+--env LD_LIBRARY_PATH=/usr/lib64:/usr/lib/x86_64-linux-gnu:/usr/local/cuda/lib64:/usr/local/cuda/lib64/stubs \
+--env PYTHONPATH=/roman_imsim \
+--env OPENBLAS_NUM_THREADS=1 \
+--env MKL_NUM_THREADS=1 \
+--env NUMEXPR_NUM_THREADS=1 \
+--env OMP_NUM_THREADS=1 \
+--env VECLIB_MAXIMUM_THREADS=1 \
+--env TERM=xterm \
+--env SNPIT_DEFAULT_CONFIG=/snpit_env/configs/nov2025_container_config.yaml \
+--env SNPIT_CONFIG=/snpit_env/configs/nov2025_container_config.yaml \
+--annotation run.oci.keep_original_groups=1 \
+-it \
+registry.nersc.gov/m4385/roman-snpit-env:cuda-dev \
+/bin/bash /home/phrosty/examples/perlmutter/20172782_slurm_demo_bashinstall.sh
--- examples/perlmutter/README.md
+++ examples/perlmutter/README.md
@@ -3,3 +3,5 @@
 Currently, this is primarily for internal development use of members of the Roman SN PIT.  You are welcome to try it if you aren't in the PIT, but at the moment we can't support you.  If you are in the PIT and have trouble with any of this, ping Rob Knop and Lauren Aldoroty in the `#photometry` channel on the Roman SNPIT Slack.
 
 For documentation on running the example in this directory, see [its writeup in the phrosty docs](https://roman-supernova-pit.github.io/phrosty/usage.html#running-on-perlmutter).
+
+A note: If you are using the slurm demo, sbatch from the directory that contains phrosty, not from within the phrosty directory itself. Go one above. When you sbatch from a particular folder, it takes that folder to be `/home` in the container. 
